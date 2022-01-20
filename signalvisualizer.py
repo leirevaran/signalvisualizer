2c766bd23c30b34bc5cc857d6ce2b8f7b0556888
Leire Varela <leirevaran@gmail.com>
2022-01-20 17:06:59 +0100

    20/01 introducing tkinter

--- signalvisualizer.py
+++ signalvisualizer.py
@@ -1,2 +1,10 @@
-# comprobando si funciona el push a github
-# comprobando si he entendido bien como funciona esto
+from tkinter import *
+
+root = Tk() # creation of the window
+root.title("Signal Visualizer") # name of the window
+root.iconbitmap("images/icon.ico") # icon at the top of the window
+root.geometry("600x300") # size of the window
+#root.config(bg="blue") # change the background color of the window
+
+# this line must always go at the bottom
+root.mainloop()
